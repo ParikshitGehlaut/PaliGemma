019111d4722223db5315a05806a87b8e247e0180
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-08-10 20:52:30 +0530

    added GemmaDecoderLayer and GemmaMLP

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -79,6 +79,54 @@ class PaliGemmaMultiModalProjector(nn.Module):
     def forward(self, x):
         return self.projector(x)
 
+class GemmaMLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = config.intermediate_size
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+
+    def forward(self, x):
+        return self.down_proj(nn.functional.gelu(self.gate_proj(x), approximate="tanh") * self.up_proj(x))
+
+class GemmaDecoderLayer(nn.Module):
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        self.hidden_size = config.hidden_size
+
+        self.self_attn = GemmaAttention(config, layer_idx)
+        self.mlp = GemmaMLP(config)
+
+        self.input_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attn_norm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        hidden_states: torch.tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+        ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+            # hidden_states : [Batch_size, seq_len, Hidden_size]
+            residual = hidden_states
+            hidden_states = self.input_layernorm(hidden_states)
+            hidden_states, _, = self.self_attn(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_mask=position_ids,
+                kv_cache=kv_cache
+            )
+            hidden_states += residual
+
+            residual = hidden_states
+            hidden_states = self.post_attn_norm(hidden_states)
+            hidden_states = self.mlp(hidden_states)
+            hidden_states += residual
+
+            return hidden_states # [Batch_size, seq_len, Hidden_size]
+
 class GemmaModel(nn.Module):
     def __init__(self, config):
         super().__init__()
